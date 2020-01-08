dcd64925683c3da04cc87c42009cf180d796ab41
skipperhc <vitorhainosz2@gmail.com>
2020-01-08 12:01:52 -0300

    indo almoçar, falta frufru

--- Desafio_Henrique/Program.cs
+++ Desafio_Henrique/Program.cs
@@ -7,7 +7,23 @@ using System.Threading.Tasks;
 namespace Desafio_Henrique {
     class Program {
         static void Main(string[] args) {
+            Veiculo veiculo= new Veiculo();
 
+            //Preenchimento
+            Console.Write("Indique a marca do veículo: ");
+            veiculo.Marca = Console.ReadLine();
+
+            Console.WriteLine("\nIndique qual o modelo do seu veículo: ");
+            veiculo.Modelo = Console.ReadLine();
+
+            Console.WriteLine("\nIndique qual a placa do seu veículo: ");
+            veiculo.Placa = Console.ReadLine();
+
+            Console.WriteLine("\nIndique quanto custa seu veículo em R$: ");
+            veiculo.Preco = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nIndique a cor do seu veículo: ");
+            veiculo.Cor = Console.ReadLine();
         }
     }
 }
--- Desafio_Henrique/Veiculo.cs
+++ Desafio_Henrique/Veiculo.cs
@@ -16,11 +16,42 @@ namespace Desafio_Henrique {
         public int Velocidade { get; set; }
         public double Preco { get; set; }
 
+        public void Ligar() {
+            if (esta_ligado == false) {
+                esta_ligado = true;
+            } else {
+                Console.WriteLine("Seu veículo já está ligado");
+            }
+            if () {
+
+            }
+        }
+
+        public void Desligar() {
+            if (esta_ligado == true && Velocidade == 0) {
+                esta_ligado = false;
+            } else if (esta_ligado == true && Velocidade > 0) {
+                Console.WriteLine("Pare o carro antes de desligar");
+            } else {
+                Console.WriteLine("Seu carro já está desligado");
+            }
+        }
+
         public void Acelerar() {
+
             if (esta_ligado == true) {
                 Velocidade = Velocidade + 20;
                 if (litros_gas > 0) {
-                    litros_gas = +1;
+                    litros_gas = -1;
+                    if (litros_gas == 0) {
+                        Console.WriteLine("Sem gasolina");
+                    }
+                    if (litros_gas < 10) {
+                        Console.WriteLine("Pouca gasolina");
+                    }
+                }
+                if (Velocidade > 140) {
+                    Console.WriteLine("Excesso de velocidade dá ruim");
                 }
             } else {
                 Console.WriteLine("o seu veículo está desligado no momento");
@@ -29,14 +60,40 @@ namespace Desafio_Henrique {
 
         public void Abastecer(int quant_litros) {
             if (litros_gas < 100) {
-                int verificar = litros_gas = + quant_litros;
+                int verificar = litros_gas = +quant_litros;
                 if (verificar > 100) {
-                    litros_gas = + quant_litros;
+                    litros_gas = +quant_litros;
                     Console.WriteLine("");
                 } else {
                     Console.WriteLine("O tanque já está cheio");
                 }
             }
+        }
+
+        public void Frear() {
+            if (esta_ligado == true && Velocidade > 0) {
+                Velocidade = -10;
+            }
+            if (Velocidade > 8000) {
+                Console.WriteLine("chama o samu");
+            }
+        }
+
+        public void Pintar(string cor_nova) {
+            if (Cor != cor_nova) {
+                Cor = cor_nova;
+            } else {
+                Console.WriteLine("Seu veículo já possui está cor");
+            }
+        }
+
+        public string Conferir_gas() {
+            switch (litros_gas) {
+                case 
+                default:
+                    break;
+            }
+            return
         }
     }
 
