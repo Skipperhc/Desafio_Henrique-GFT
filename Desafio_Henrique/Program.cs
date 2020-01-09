53bdd913a61b011e2c0500a2b5eae856aa579017
skipperhc <vitorhainosz2@gmail.com>
2020-01-09 14:29:30 -0300

    terminado, eu acho

--- Desafio_Henrique/Program.cs
+++ Desafio_Henrique/Program.cs
@@ -8,6 +8,7 @@ namespace Desafio_Henrique {
     class Program {
         static void Main(string[] args) {
             Veiculo veiculo= new Veiculo();
+            Boolean verificador = false;
 
             //Preenchimento
             Console.Write("Indique a marca do veículo: ");
@@ -25,6 +26,20 @@ namespace Desafio_Henrique {
             Console.Write("\nIndique a cor do seu veículo: ");
             veiculo.Cor = Console.ReadLine();
 
+            while (verificador == false) {
+                Console.Write("\nIndique a quantidade de gasolina: ");
+                int gas = veiculo.Litros_gas = Convert.ToInt32(Console.ReadLine());
+
+                if (gas <= 100) {
+                    veiculo.Litros_gas = gas;
+                    verificador = true;
+                } else {
+                    Console.WriteLine("Insira uma quantidade inferior a 100 L");
+                    verificador = false;
+                }
+            }
+
+
             Console.Clear();
             Console.WriteLine($"Status: \n\n" +
                 $"\nMarca: " + //8, 3
@@ -35,6 +50,7 @@ namespace Desafio_Henrique {
                 $"\nVelocidade: " +//13, 8
                 $"\nLitros de gasolina: " +//21, 9
                 $"\nKM: ");//5, 10
+            veiculo.Atualizar();
 
             veiculo.Writeat(8,3, veiculo.Marca);
             veiculo.Writeat(9,4, veiculo.Modelo);
@@ -48,36 +64,51 @@ namespace Desafio_Henrique {
             Console.WriteLine("\nO que você deseja: ");
             Console.WriteLine($"\n" +
                 $"01 - Ligar    | 02 - Desligar | 03 - Abastecer" +
-                 "\n04 - Acelerar | 05 - Frear    | 06 - Pintar\n");
+                 "\n04 - Acelerar | 05 - Frear    | 06 - Pintar\n" +
+                 "Opção: ");
 
 
             while (true) {
                 
                 string opcao = Console.ReadLine();
-
+                if (opcao == null) {
+                    Console.WriteLine("po bicho");
+                    break;
+                }
                 switch (Convert.ToInt32(opcao)) {
                     case 01: veiculo.Ligar();
+                        veiculo.zerarperg();
                         break;
                     case 02: veiculo.Desligar();
+                        veiculo.zerarperg();
                         break;
                     case 03:
+                        Console.SetCursorPosition(0, 17);
                         Console.WriteLine("quanto deseja abastecer?");
                         string quant = Console.ReadLine();
                         veiculo.Abastecer(Convert.ToInt32(quant));
+                        veiculo.zerarperg();
                         break;
                     case 04: veiculo.Acelerar();
+                        veiculo.zerarperg();
                         break;
                     case 05: veiculo.Frear();
+                        veiculo.zerarperg();
                         break;
                     case 06:
                         Console.WriteLine("Qual a Cor que deseja?");
                         string CorNova = Console.ReadLine();
                         veiculo.Pintar(CorNova);
+                        veiculo.zerarperg();
                         break;
                     default:
                         Console.WriteLine("opcao indisponível \n Insira uma opcao existente");
+                        veiculo.zerarperg();
                         break;
                 }
+
+                //Console.SetCursorPosition(); 17
+
             }
 
         }
--- Desafio_Henrique/Veiculo.cs
+++ Desafio_Henrique/Veiculo.cs
@@ -19,30 +19,36 @@ namespace Desafio_Henrique {
         public void Ligar() {
             if (esta_ligado == false) {
                 Console.WriteLine("Seu carro está Ligado");
-                Pausinha();
+                esta_ligado = true;
+                Atualizar();
                 esta_ligado = true;
             } else {
                 Console.WriteLine("Seu veículo já está ligado");
             }
+            Pausinha();
         }
 
         public void Desligar() {
             if (esta_ligado == true && Velocidade == 0) {
                 Console.WriteLine("Seu carro está desligado");
                 esta_ligado = false;
+                Atualizar();
             } else if (esta_ligado == true && Velocidade > 0) {
                 Console.WriteLine("Pare o carro antes de desligar");
             } else {
                 Console.WriteLine("Seu carro já está desligado");
             }
+            Pausinha();
         }
 
         public void Acelerar() {
 
             if (esta_ligado == true && Conferir_gas() != "vazio") {
                 Velocidade = Velocidade + 20;
-                if (Conferir_gas() == "cheio") {
-                    Litros_gas = -1;
+                Litros_gas = Litros_gas - 1;
+                Atualizar();
+                if (Conferir_gas() == "cheio" || Conferir_gas() == "abastecido" || Conferir_gas() == "baixo") {
+                    Atualizar();
                     if (Conferir_gas() == "vazio") {
                         Console.WriteLine("Sem gasolina");
                     }
@@ -56,19 +62,21 @@ namespace Desafio_Henrique {
             } else {
                 Console.WriteLine("o seu veículo está desligado no momento");
             }
+            Pausinha();
         }
 
         public void Abastecer(int quant_litros) {
             if (Litros_gas < 100) {
                 int verificar = Litros_gas + quant_litros;
-                if (verificar < 100) {
-                    Litros_gas =+ quant_litros;
-                    Writeat(21, 9, Convert.ToString(Litros_gas));
+                if (verificar <= 100) {
+                    Litros_gas = Litros_gas + quant_litros;
+                    Atualizar();
                     Console.WriteLine("");
-                } else {
-                    Console.WriteLine("O tanque já está cheio");
+                } else if (verificar > 100) {
+                    Console.WriteLine("\nO tanque já está cheio");
                 }
             }
+            Pausinha();
         }
 
         public void Frear() {
@@ -89,12 +97,13 @@ namespace Desafio_Henrique {
         }
 
         public string Conferir_gas() {
+            //estou achando um metodo inutil
             string nivel = "";
             if (Litros_gas == 100) {
                 nivel = "cheio";
-            } else if (Litros_gas < 50) {
-                nivel = "metade";
-            } else if (Litros_gas < 10) {
+            } else if (Litros_gas > 10 && Litros_gas < 50) {
+                nivel = "abastecido";
+            } else if (Litros_gas < 10 && Litros_gas > 0) {
                 nivel = "baixo";
             } else if (Litros_gas == 0) {
                 nivel = "vazio";
@@ -108,6 +117,40 @@ namespace Desafio_Henrique {
         public void Writeat(int left, int top, string frase) {
             Console.SetCursorPosition(left, top);
             Console.Write(frase);
+        }
+
+        public void Writeatgas(int left, int top, int gas) {
+            Console.SetCursorPosition(left, top);
+            Console.Write(gas);
+        }
+
+        public void Atualizar() {
+            Writeat(8, 3, Marca);
+            Writeat(9, 4, Modelo);
+            Writeat(8, 5, Convert.ToString(Preco));
+            Writeat(8, 6, Placa);
+            Writeat(6, 7, Cor);
+            Writeat(13, 8, Convert.ToString(Velocidade));
+            Writeat(21, 9, "    ");
+            Writeatgas(21, 9, Litros_gas);
+            Writeat(5, 10, Convert.ToString(Km));
+            string status = "";
+            if (esta_ligado == true) {
+                status = "ligado                       ";
+            } else {
+                status = "Desligado                     ";
+            }
+            Writeat(9, 0, status);
+        }
+
+        public void zerarperg() {
+            for (int j = 16; j < 23; j++) {
+                for (int i = 0; i < 40; i++) {
+                    Console.SetCursorPosition(i, j);
+                    Console.Write(" ");
+                }
+            }
+            Console.SetCursorPosition(0, 16);
         }
     }
 
